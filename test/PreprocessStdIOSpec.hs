8ed1d6729705b4da8bbb70a92e1f7e8403506383
Harry Garrood <harry@garrood.me>
2014-07-01 01:48:46 -0700

    Add tests for scss/sass

--- test/PreprocessStdIOSpec.hs
+++ test/PreprocessStdIOSpec.hs
@@ -7,3 +7,9 @@ spec :: Spec
 spec = do
     it "should use stdIO" $ do
         testWithExpectedResult "hello.txt"
+
+    it "should use scss" $ do
+        testWithExpectedResult "blue.css"
+
+    it "should use sass" $ do
+        testWithExpectedResult "red.css"
