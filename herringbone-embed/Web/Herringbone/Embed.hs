896f61db0295111190fcbd14f559c7e7ef95ab20
Harry Garrood <harry@garrood.me>
2014-07-25 23:54:03 -0700

    code cleanup in herringbone-embed

--- herringbone-embed/Web/Herringbone/Embed.hs
+++ herringbone-embed/Web/Herringbone/Embed.hs
@@ -15,14 +15,20 @@ import Web.Herringbone
 -- in a Template Haskell splice. Any asset compilation failures will result in
 -- a compile error.
 --
--- The result will be a value of type @[(LogicalPath, ByteString)]@; that is, a
--- mapping of logical paths to file contents.
+-- For example:
+--
+-- > assets :: [(LogicalPath, ByteString)]
+-- > assets = $(embedAssets
+--      (herringbone
+--          (setSourceDir "assets" . setDestDir ".compiled_assets"))
+--      )
 embedAssets :: IO Herringbone -> Q Exp
 embedAssets iohb = do
     hb <- runIO iohb
     errs <- runIO (precompile hb)
-    when (not (null errs)) $
-        fail ("asset compilation failed: " ++ concatMap show errs)
+    when (not . null $ errs) $
+        fail ("the following assets failed to compile:\n" ++
+            unlines (map show errs))
 
     SigE expr' _ <- embedDir (F.encodeString $ hbDestDir hb)
     let expr = transformFiles expr'
@@ -30,29 +36,16 @@ embedAssets iohb = do
     type_ <- [t| [(LogicalPath, ByteString)] |]
     return $ SigE expr type_
     where
-    logicalPathToExp logicalPath =
-        AppE
-            (VarE 'unsafeMakeLogicalPath)
-            (ListE (map (LitE . StringL . T.unpack)
-                        (fromLogicalPath logicalPath)))
-    errToExp e = case e of
-        AssetNotFound ->
-            ConE 'AssetNotFound
-        AssetCompileError err ->
-            AppE (ConE 'AssetCompileError) $ compileErrToExp err
-        AmbiguousSources srcs ->
-            AppE (ConE 'AmbiguousSources) $ sourcesToExp srcs
-    compileErrToExp err = LitE (StringL (B8.unpack err))
-    sourcesToExp srcs = ListE $ map filePathToExp srcs
-    -- Just use a literal because we have an IsString instance
-    filePathToExp path = LitE (StringL (F.encodeString path))
-
     transformFiles (ListE tups) =
         let f (TupE [LitE (StringL path), contents]) =
                 TupE [logicalPathExp path, contents]
             f _ = error "unexpected Exp in precompileEmbed"
         in ListE $ map f tups
     transformFiles _ = error "unexpected Exp in precompileEmbed"
-
     logicalPathExp = logicalPathToExp . stringToLogicalPath
     stringToLogicalPath = unsafeMakeLogicalPath . T.splitOn "/" . T.pack
+    logicalPathToExp logicalPath =
+        AppE
+            (VarE 'unsafeMakeLogicalPath)
+            (ListE (map (LitE . StringL . T.unpack)
+                        (fromLogicalPath logicalPath)))
